f248380658298d5f2106d097d19297aca8ea365f
Aiden <133554498+Electric-Lobster@users.noreply.github.com>
2025-10-30 16:35:29 -0400

    more counter code

--- src/Elements/Counter.js
+++ src/Elements/Counter.js
@@ -1,16 +1,28 @@
 class Counter extends HTMLElement {
 
-    static observedAttributes = [];
-
     constructor() {
         super();
     }
 
+    static get observedAttributes() {
+        return ["start", "end"];
+    };
+
+    attributechangedCallback(name, oldValue, newValue) {
+        this.render();
+    }
+
     connectedCallback() {
+        setInterval(this.render, 1000);
         this.render();
     }
 
     render() {
+
+        const countdowndates = [new Date(this.getAttribute("start")).getTime(), new Date(this.getAttribute("end")).getTime()];
+
+
+
         this.innerHTML = `<div class="mainPoint">
             <h1>&nbsp;</h1>
             <h3>Until Out Next Event</h3>
