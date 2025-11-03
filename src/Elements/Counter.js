cfc501d4b22402c45e2cedb08fd3b7b2cbde4d8e
Consistent-Orange <jcreutter2@gmail.com>
2025-11-03 17:05:11 -0500

    Squashed commit of the following:
    commit e6cc6e684a4313d4db8bf7a7e963fdf8dba71aa2
    Author: Aiden <133554498+Electric-Lobster@users.noreply.github.com>
    Date:   Fri Oct 31 13:16:01 2025 -0400
    
        more stufffs
    
    commit 1d9c61895168cdf31ca2020b07bc1213563c0763
    Author: Aiden <133554498+Electric-Lobster@users.noreply.github.com>
    Date:   Fri Oct 31 12:39:14 2025 -0400
    
        adjusted counter
    
    commit ff59414c4f1dcd762dee5859b03680df5550fc8d
    Author: Aiden <133554498+Electric-Lobster@users.noreply.github.com>
    Date:   Fri Oct 31 10:38:21 2025 -0400
    
        fixed counter again
    
    commit fae203d01c89e4ae4f1775726d2b972694823ccd
    Author: Aiden <133554498+Electric-Lobster@users.noreply.github.com>
    Date:   Fri Oct 31 10:04:16 2025 -0400
    
        added header element
    
    commit ccaadf75aaf3b9a317176b36c0fc82f3fcc53656
    Author: Aiden <133554498+Electric-Lobster@users.noreply.github.com>
    Date:   Fri Oct 31 09:07:28 2025 -0400
    
        added pages
    
    commit 9590553eef7eb38f8d7d2f32ac1f3f4fc438ab1e
    Author: Aiden <133554498+Electric-Lobster@users.noreply.github.com>
    Date:   Fri Oct 31 08:53:49 2025 -0400
    
        more stuff
    
    commit cddd8602e6f652d6686e7a552de06c388a793bc9
    Author: Aiden P <Aidenpepoli@icloud.com>
    Date:   Thu Oct 30 20:58:32 2025 -0400
    
        removed old counter
    
    commit e296a8afe7871a87e6f70ca03b990ea479b15490
    Author: Aiden P <Aidenpepoli@icloud.com>
    Date:   Thu Oct 30 20:51:35 2025 -0400
    
        finished counter
    
    commit c57f5fd9846953abcf299c718057ace9aa803b42
    Author: Aiden P <Aidenpepoli@icloud.com>
    Date:   Thu Oct 30 19:19:53 2025 -0400
    
        working counter
    
    commit c5c3434d698f9338b97414c73f16a409e6682e31
    Author: Aiden P <Aidenpepoli@icloud.com>
    Date:   Thu Oct 30 18:04:57 2025 -0400
    
        More stuff
    
    commit 066effc7073fd237346d61f000f6852ca55f09f4
    Author: Aiden <133554498+Electric-Lobster@users.noreply.github.com>
    Date:   Thu Oct 30 17:00:45 2025 -0400
    
        working counter thiung
    
    commit f248380658298d5f2106d097d19297aca8ea365f
    Author: Aiden <133554498+Electric-Lobster@users.noreply.github.com>
    Date:   Thu Oct 30 16:35:29 2025 -0400
    
        more counter code
    
    commit f8d3127dae3ef01e087c2b98af74545504d0ccea
    Author: Aiden <133554498+Electric-Lobster@users.noreply.github.com>
    Date:   Thu Oct 30 13:32:37 2025 -0400
    
        more changes
    
    commit 2e5ad6d851303f9b4cd38de42345485dae067e00
    Author: Aiden <133554498+Electric-Lobster@users.noreply.github.com>
    Date:   Thu Oct 30 12:47:42 2025 -0400
    
        Moved main page

---
+++ src/Elements/Counter.js
@@ -0,0 +1,61 @@
+class Counter extends HTMLElement {
+
+    static startTime = 0;
+    static endTime = 0;
+
+    constructor() {
+        super();
+
+
+        this.startTime = new Date( this.getAttribute("start")).getTime()
+        this.endTime = new Date( this.getAttribute("end")).getTime()
+    }
+
+    static get observedAttributes() {
+        return ["start", "end"];
+    };
+
+    attributechangedCallback(name, oldValue, newValue) {
+
+        console.log(name)
+    }
+
+    connectedCallback() {
+        this.render();
+        this.setCounter();
+    }
+
+    render() {
+
+        this.innerHTML = `<div class="mainPoint">
+        <h1 id="countdown">&nbsp;</h1>
+        <h3> <a id="label" href="pages/events.html"></a> </h3>
+        </div>`
+                          
+        setInterval(() => {
+            this.setCounter();
+        }, 1000);
+    }
+
+    setCounter() {
+        const now = new Date().getTime()
+
+        const distanceStart = this.startTime - now
+        const distanceEnd = this.endTime - now
+
+        const beforeEvent = distanceStart > 0
+
+        const distance = beforeEvent ? distanceStart : distanceEnd
+
+        const time = [Math.floor(distance / (1000 * 60 * 60 * 24)),
+            Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+            Math.floor((distance % (1000 * 60)) / 1000)]
+
+        this.querySelector('#countdown').innerHTML = `<b>${time[0]} Days, ${time[1]} Hours, ${time[2]} Minutes, ${time[3]} Seconds</b>`;
+        this.querySelector('#label').innerHTML = beforeEvent ? "Until Our Next Event" : "Until Event Ends"
+    }
+
+}
+
+customElements.define('test-counter', Counter);
---
+++ src/Elements/Header.js
@@ -0,0 +1,26 @@
+class WebHeader extends HTMLElement {
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    static nav = `<nav>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="blog.html">Blog</a>
+        <a href="contact.html">Contact</a>
+        <a href="events.html">Events</a>
+    </nav>`;
+
+    render() {
+        this.innerHTML = `<header>
+            ${WebHeader.nav}
+        </header>`;
+    }
+}
+
+customElements.define('web-header', WebHeader, { extends: "header" });
