9808f308ccf60374f19c990a39753412d261d2bb
Consistent-Orange <jcreutter2@gmail.com>
2025-11-17 19:33:03 -0500

    Minor amendments to style

--- Elements/Header.js
+++ Elements/Header.js
@@ -25,12 +25,12 @@
 
 customElements.define('web-header', WebHeader, { extends: "header" });*/
 let header = document.createElement("header");
-header.innerHTML = `<nav>
-        <a href="index.html">Home</a>
-        <a href="about.html">About</a>
-        <a href="blog.html">Blog</a>
-        <a href="contact.html">Contact</a>
-        <a href="events.html">Events</a>
+header.innerHTML = `<nav id="darkshade">
+        <a href="index.html" class="glassMorph">Home</a>
+        <a href="about.html" class="glassMorph">About</a>
+        <a href="blog.html" class="glassMorph">Blog</a>
+        <a href="contact.html" class="glassMorph">Contact</a>
+        <a href="events.html" class="glassMorph">Events</a>
     </nav>`;
 header.id = "headpic";
 document.body.insertBefore(header, document.getElementById("main"))
